af08ca1ed03b071f4263a1e426da01ba040356e3
Minho Ryang <minhoryang@gmail.com>
2020-01-19 19:51:39 +0900

    With hostname

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
+import os
+
 from flask import Flask, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-
+hostname = os.environ.get('HOSTNAME', '--no-hostname--')
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def main():
-    return f"Hello {request.base_url}"
+    return f"Hello {request.base_url} from {hostname}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
