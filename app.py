eef5f95e7f80c2b010cc1c7ac402f014964330cd
Minho Ryang <minho@comcom.ai>
2020-05-19 12:12:21 +0900

    Hi Develop

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def main():
-    return f"Hello {request.base_url} from {hostname}"
+    return f"Hello {request.base_url} from {hostname} (Hi I'm Develop)"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
